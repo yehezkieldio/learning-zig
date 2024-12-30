82eb5b5f61481b70e18b4a7ceebd1f58b0c6f8a1
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-12-30 08:30:37 +0800

    feat: for loop

---
+++ for.zig
@@ -0,0 +1,18 @@
+test "for" {
+    // character literals are equivalent to integer literals
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string, 0..) |character, index| {
+        _ = character;
+        _ = index;
+    }
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string, 0..) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
